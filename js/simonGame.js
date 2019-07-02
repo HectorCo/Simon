47983f6edeaf27e523439ad0fdae0d75a23392e7
HectorCo <hcompanyortega@gmail.com>
2019-07-02 16:57:34 +0200

    Finalizado 1.0

--- js/simonGame.js
+++ js/simonGame.js
@@ -1,11 +1,5 @@
 // Creamos las variables globales
 var numeros = [];
-if (numeros.length>0){
-  numeros = [];
-}
-
-console.log(numeros);
-
 var puntuacion = 0;
 var pos = 0;
 var ciclo = "maquina";
@@ -14,24 +8,14 @@ var ciclo = "maquina";
 /* Cuando se hace un click al boton de empezar se inicia la funcion que nos sirve tan para iniciar el
 juego como para resetearlo */
 
-document.getElementById("start").onclick = function (){
-  
-  console.log(numeros);
-  alert('Antes de Vaciar');
-
-  //numeros.splice(0, numeros.length);
-    while(numeros.length > 0) { numeros.pop();  }
-  
-  console.log(numeros);
-  alert('Despues de Vaciar');
-
+document.getElementById("start").onclick = function () {
+  numeros.splice(0, numeros.length);
   generarObjeto();
   pos = 0;
   let puntuacion = 0;
   document.getElementById('contador').innerHTML = "0" + puntuacion;
   document.getElementById('start').innerHTML = "RESET";
-  console.log(numeros);  
-  ciclo="maquina";
+  ciclo = "maquina";
   play();
 };
 
@@ -42,15 +26,15 @@ que regulan el comportamiento de los botones  */
 document.getElementById("rojo").onclick = function rojoF() {
   document.getElementById("rojo").style.opacity = "1";
   document.getElementById("audioBotonRojo").play();
-  setTimeout(function() {
+  setTimeout(function () {
     document.getElementById("rojo").style.opacity = "0.5";
   }, 500);
 
   if (ciclo == "usuario") {
-    
+
     if (numeros[pos] == 1) {
       pos++;
-      
+
 
       if (pos == numeros.length) {
         ciclo = "maquina";
@@ -58,31 +42,28 @@ document.getElementById("rojo").onclick = function rojoF() {
         generarObjeto();
         pos = 0;
         setTimeout(play, 1500);
-      } else {
-      }
+      } else {}
     } else {
       document.getElementById('contador').innerHTML = "0" + puntuacion;
       document.getElementById('start').innerHTML = "START";
-      
+
       alert("GAME OVER");
-      
+
     }
-  } else {
-  }
+  } else {}
 };
 
 document.getElementById("verde").onclick = function verdeF() {
   document.getElementById("verde").style.opacity = "1";
   document.getElementById("audioBotonVerde").play();
-  setTimeout(function() {
+  setTimeout(function () {
     document.getElementById("verde").style.opacity = "0.5";
   }, 500);
 
   if (ciclo == "usuario") {
-    console.log(ciclo);
     if (numeros[pos] == 2) {
       pos++;
-      
+
 
       if (pos == numeros.length) {
         ciclo = "maquina";
@@ -90,29 +71,27 @@ document.getElementById("verde").onclick = function verdeF() {
         generarObjeto();
         pos = 0;
         setTimeout(play, 1500);
-      } else {
-      }
+      } else {}
     } else {
       document.getElementById('contador').innerHTML = "0" + puntuacion;
       document.getElementById('start').innerHTML = "START";
       alert("GAME OVER");
     }
-  } else {
-  }
+  } else {}
 };
 
 document.getElementById("azul").onclick = function azulF() {
   document.getElementById("azul").style.opacity = "1";
   document.getElementById("audioBotonAzul").play();
-  setTimeout(function() {
+  setTimeout(function () {
     document.getElementById("azul").style.opacity = "0.5";
   }, 500);
 
   if (ciclo == "usuario") {
-    console.log(ciclo);
+
     if (numeros[pos] == 3) {
       pos++;
-      
+
 
       if (pos == numeros.length) {
         ciclo = "maquina";
@@ -120,29 +99,27 @@ document.getElementById("azul").onclick = function azulF() {
         generarObjeto();
         pos = 0;
         setTimeout(play, 1500);
-      } else {
-      }
+      } else {}
     } else {
       document.getElementById('contador').innerHTML = "0" + puntuacion;
       document.getElementById('start').innerHTML = "START";
       alert("GAME OVER");
     }
-  } else {
-  }
+  } else {}
 };
 
 document.getElementById("amarillo").onclick = function amarilloF() {
   document.getElementById("amarillo").style.opacity = "1";
   document.getElementById("audioBotonAmarillo").play();
-  setTimeout(function() {
+  setTimeout(function () {
     document.getElementById("amarillo").style.opacity = "0.5";
   }, 500);
 
   if (ciclo == "usuario") {
-    console.log(ciclo);
+
     if (numeros[pos] == 4) {
       pos++;
-      
+
 
       if (pos == numeros.length) {
         ciclo = "maquina";
@@ -150,15 +127,13 @@ document.getElementById("amarillo").onclick = function amarilloF() {
         generarObjeto();
         pos = 0;
         setTimeout(play, 1500);
-      } else {
-      }
+      } else {}
     } else {
       document.getElementById('contador').innerHTML = "0" + puntuacion;
       document.getElementById('start').innerHTML = "START";
       alert("GAME OVER");
     }
-  } else {
-  }
+  } else {}
 };
 
 /* Función para generar números aleatotios entre 1 y 4 */
@@ -170,7 +145,7 @@ function generarObjeto() {
 // Funcion que recorre el array y reproduce la secuencia de sonidos
 
 function play() {
-  console.log(numeros); 
+
   if (pos < numeros.length) {
     switch (numeros[pos]) {
       case 1:
